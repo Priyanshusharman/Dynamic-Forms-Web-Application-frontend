cdf5aab3b438e966a1e4710869009ebb1f28beb5
priyanshusharman <priyanshuk1900@gmaul.com>
2024-06-17 10:51:09 +0530

    Initialize project using Create React App

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('Form', 'root', 'priyanshu', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
